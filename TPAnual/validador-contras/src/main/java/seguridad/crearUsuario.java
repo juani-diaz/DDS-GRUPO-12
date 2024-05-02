aea21c77685d2ce5500087a277d394c997f00e46
Juan-Polito <juanpolito02@gmail.com>
2024-05-01 22:03:12 -0300

    no funca
    Crear archivo no encuentra el path en la linea 69 cuando se cambaron los nombres de las carpetas

--- TPAnual/validador-contras/src/main/java/seguridad/crearUsuario.java
+++ TPAnual/validador-contras/src/main/java/seguridad/crearUsuario.java
@@ -66,7 +66,7 @@ public class crearUsuario {
   }
   public static boolean comparar_contra_contra_top10000(String contra){
     try {
-      File archivo_txt = new File("TPAnual/top10000_Peores_Contras.txt");
+      File archivo_txt = new File("seguridad/top10000_Peores_Contras.txt");
       Scanner myReader = new Scanner(archivo_txt);
 
       while (myReader.hasNextLine()) {
