a0a71c8c635835ed92841e1393dd1c3a697fa5ed
Juan-Polito <juanpolito02@gmail.com>
2024-06-04 14:25:42 -0300

    NUevos metodos

---
+++ TPAnual/validador-contras/src/main/java/domain/heladera/EnumEstadoHeladera.java
@@ -0,0 +1,6 @@
+package domain.heladera;
+
+public enum EnumEstadoHeladera {
+    DISPONIBLE,
+    TEMPERATURA_FUERA_DE_RANGO
+}
--- TPAnual/validador-contras/src/main/java/domain/personas_empresas/PersonaHumana.java
+++ TPAnual/validador-contras/src/main/java/domain/personas_empresas/PersonaHumana.java
@@ -3,27 +3,18 @@ package domain.personas_empresas;
 import java.time.LocalDate;
 import domain.personas_empresas.PersonaVulnerable;
 import java.util.List;
-public class PersonaHumana {
+public class PersonaHumana extends PersonaColaboradora {
     private String nombre;
     private String apellido;
     private LocalDate fechaNacimiento; //como se pondria aca un valor nulo?
     private String descripcionDeColaboracion;
     private List<Tarjeta> tarjetasParaEntregar;
 
-    public void tarjetaParaVulnerable(Tarjeta tarjeta){
-        // nose muy bien como implementarlo yo pensaba algo asi como un setter de la tarjeta
-        // a persona vulnerable algo asi, fijense si esta bien
-        // PersonaVulnerable.setTarjeta(Tarjeta);
-        this.tarjetasParaEntregar.add(tarjeta);
-    }
-//    public static tarjetasParaEntregar(List<Tarjeta> tarjetasNuevas){
-//        List<String> union = new ArrayList<Tarjeta>();
-//        union.addAll(tarjetasParaEntregar);
-//        union.addAll(tarjetasNuevas);
-//        setTarjetasParaEntregar(union);
-//    }
-
-    public void setTarjetasParaEntregar(List<Tarjeta> tarjetasParaEntregar) {
-        this.tarjetasParaEntregar = tarjetasParaEntregar;
+    public boolean tarjetaParaVulnerable(PersonaVulnerable destinatario){
+        if(!tarjetasParaEntregar.isEmpty()) {
+            destinatario.setTarjeta(tarjetasParaEntregar.remove(0));
+            return true;
+        }
+        return false;
     }
 }
