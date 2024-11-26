e6571dac312cd76533da8ef13b8cf358842082fd
Juan-Polito <juanpolito02@gmail.com>
2024-11-26 11:58:03 -0300

    Avances en HeladeraController
    Se emprolija el codigo. Se saca lo que no se usa y se hacen funciones reutilizables. Ademas, ya no se hardcodea el ID heladera para buscarla en la BD y se crea la relacion en BD heladera_vianda

--- TPAnual/validador-contras/src/main/java/controllers/HeladeraController.java
+++ TPAnual/validador-contras/src/main/java/controllers/HeladeraController.java
@@ -18,42 +18,16 @@ import java.util.List;
 
 public class HeladeraController {
 
-    //Como no está el ORM tenemos que usar algo para guardar
-    private static Map<String, Colaborador> colaboradores = new HashMap<>();
-    private static Map<String, Heladera> heladeras = new HashMap<>();
+      private static Heladera obtenerHeladera(EntityManager em,Long heladeraID_Long) {
 
-    public Vianda sacarVianda(Context ctx) {
-        String heladeraId = ctx.formParam("heladeraId");
-        int indice = Integer.parseInt(ctx.formParam("indice"));
+        Heladera heladera = null;
 
-        Heladera heladera = HeladeraController.obtenerHeladera(heladeraId);
-
-        //Buscar la heladera en el almacenamiento en memoria
-        //Heladera heladera = heladeras.get(heladeraId);
-
-        // Validación: verificar que la heladera exista
-       // if (heladera == null) {
-        //    ctx.result("Heladera no encontrada");
-        //    return null;
-        //}
-        // Sacar la vianda en el índice especificado
-        Vianda vianda = heladera.sacarVianda(indice);
-
-        // Retornar la vianda sacada y enviar una respuesta
-        ctx.result("Vianda retirada exitosamente.");
-        return vianda;
-    }
-
-      private static Heladera obtenerHeladera(String heladeraId) {
-        //Buscar la heladera en el almacenamiento en memoria
-        Heladera heladera = heladeras.get(heladeraId);
-
-        // Validación: verificar que la heladera exista
-        if (heladera == null) {
-          System.out.println("la heladera" + heladeraId + "no existe");
-          return null;
+        try {
+          heladera = em.getReference(Heladera.class, heladeraID_Long);
+        } catch (Exception e) {
+          System.out.println("Error al agregar la heladera: " + e + " en HeladeraController.obtenerHeladera");
         }
-        return heladera; // Simulación: obtén la heladera desde la base de datos o un repositorio
+        return heladera;
     }
 
     // Metodo para agregar una vianda a la heladera
@@ -62,38 +36,33 @@ public class HeladeraController {
 
       // Obtener parámetros del formulario (datos enviados en la solicitud)
       String comida = ctx.formParam("comida");
-      System.out.println(comida);
       String fechaVencimientoStr = ctx.formParam("fechaVencimiento");
       String fechaDonacionStr = ctx.formParam("fechaDonacion");
       String calorias = ctx.formParam("calorias");
       String pesoStr = ctx.formParam("peso");
-      //String colaboradorId = ctx.formParam("colaboradorId");
       String heladeraId = ctx.formParam("heladeraId");
+
       //String estadoStr = ctx.formParam("estado");
+      //String colaboradorId = ctx.formParam("colaboradorId");
 
       // Convertir parámetros necesarios
       LocalDate fechaVencimiento = LocalDate.parse(fechaVencimientoStr);
       LocalDate fechaDonacion = LocalDate.parse(fechaDonacionStr);
       float peso = Float.parseFloat(pesoStr);
+      Long heladeraID_Long = Long.parseLong(heladeraId);
       EnumEstadoVianda estado = EnumEstadoVianda.NO_ENTREGADO;
 
-
-      // Buscar la heladera en el almacenamiento en BD
+      //Se crea el EntityManager
       EntityManager em = BDUtils.getEntityManager();
       BDUtils.comenzarTransaccion(em);
-      Heladera heladera = HeladeraController.obtenerHeladera(heladeraId);
-      try {
-        System.out.println("try  1111");
-        heladera = em.find(Heladera.class, heladeraId);
-      } catch (Exception e) {
-        System.out.println("Error al agregar la heladera: " + e);
-      }
+
+      // Busca la heladera en la BD
+      Heladera heladera = HeladeraController.obtenerHeladera(em, heladeraID_Long);
 
       Vianda vianda = new Vianda(comida, fechaVencimiento, fechaDonacion, calorias, peso, estado);
       vianda.setHeladera(heladera);
 
       try {
-        System.out.println("try  2");
         em.persist(vianda);
       } catch (Exception e) {
         System.out.println("Error al agregar la vianda: " + e);
@@ -103,6 +72,26 @@ public class HeladeraController {
 
       //ctx.result("Vianda agregada exitosamente.");
     }
+
+ /*public Vianda sacarVianda(Context ctx) {
+        String heladeraId = ctx.formParam("heladeraId");
+        int indice = Integer.parseInt(ctx.formParam("indice"));
+
+        //Buscar la heladera en el almacenamiento en memoria
+        Heladera heladera = heladeras.get(heladeraId);
+
+        // Validación: verificar que la heladera exista
+        if (heladera == null) {
+            ctx.result("Heladera no encontrada");
+            return null;
+        }
+        // Sacar la vianda en el índice especificado
+        Vianda vianda = heladera.sacarVianda(indice);
+
+        // Retornar la vianda sacada y enviar una respuesta
+        ctx.result("Vianda retirada exitosamente.");
+        return vianda;
+    }*/
 
     //Metodo para obtener el colaborador por su ID
     //La idea es implementar esto para cuando haya una base de datos
--- TPAnual/validador-contras/src/main/java/domain/heladera/Heladera.java
+++ TPAnual/validador-contras/src/main/java/domain/heladera/Heladera.java
@@ -29,7 +29,7 @@ public class Heladera {
   private LocalDate fechaFuncionamiento;
 
   @OneToMany//(cascade = CascadeType.ALL)
-  @JoinColumn(name = "heladera")
+  //@JoinColumn(name = "heladera")
   private List<Vianda> viandasEnHeladera=new ArrayList<>();
 
   private Float temperaturaMinima;
