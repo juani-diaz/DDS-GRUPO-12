9bd5defa2514faf5d3fcf01ec747caf703f42199
juani-diaz <juanidiaz8260@gmail.com>
2024-12-08 23:27:16 -0300

    fix

--- TPAnual/validador-contras/src/main/java/persistence/Repos/RepoColaborador.java
+++ TPAnual/validador-contras/src/main/java/persistence/Repos/RepoColaborador.java
@@ -26,21 +26,34 @@ public class RepoColaborador {
                                                 "JOIN c.persona p  )"
         );
 
-        Query query2 = em.createQuery("SELECT p.id, COUNT (DISTINCT d) FROM DonacionVianda d JOIN d.colaborador c JOIN ersona pGROUP BY p");
+        Query query2 = em.createQuery("SELECT p.id, COUNT (DISTINCT d) FROM DonacionVianda d JOIN d.colaborador c JOIN c.persona p GROUP BY p");
 
 
+        System.out.println("Query creada: " + query1);
         System.out.println("Query creada: " + query2);
 
-        List<Object[]> heladeras = query2.getResultList();
+        List<Object[]> personasFisicas = query1.getResultList();
+        List<Object[]> donaciones = query2.getResultList();
+
+
+        System.out.println(personasFisicas);
+        System.out.println(donaciones);
 
         List<Map<String, Object>> resultado = new ArrayList<>();
-        for (Object[] fila : heladeras) {
+        for (Object[] fila1 : personasFisicas) {
             Map<String, Object> mapa = new HashMap<>();
-            mapa.put("id", fila[0]);
-            mapa.put("nombre", fila[1]);
-            mapa.put("apellido", fila[2]);
-            resultado.add(mapa);
+
+
+            for (Object[] fila2:donaciones){
+                if(fila1[0] == fila2[0]){ //comparo ids y si son iguales los agrego al mapa para devolverlo
+            mapa.put("nombre", fila1[1]);
+            mapa.put("apellido", fila1[2]);
+            mapa.put("donacion",fila2[1]);
+            resultado.add(mapa);}
         }
+        }
+
+
 
         return resultado;
     }
