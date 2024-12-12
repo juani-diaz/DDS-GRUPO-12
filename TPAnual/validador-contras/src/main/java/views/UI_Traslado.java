2e1b2070ad5679e6d0e55de7bd000aed7063a5f9
JuanMTerrizzi <jterrizzi@frba.utn.edu.ar>
2024-12-12 12:18:18 -0300

    Funcion trasladar

--- TPAnual/validador-contras/src/main/java/views/UI_Traslado.java
+++ TPAnual/validador-contras/src/main/java/views/UI_Traslado.java
@@ -10,7 +10,9 @@ import persistence.Repos.RepoHeladera;
 import persistence.Repos.RepoVianda;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UI_Traslado extends UI_Navegable implements Handler{
@@ -36,16 +38,44 @@ public class UI_Traslado extends UI_Navegable implements Handler{
     // Obtener parámetros del formulario (datos enviados en la solicitud)
     String cantidad = ctx.formParam("cantidad");
     System.out.println("cantidad= "+cantidad);
-    String heladeraId = ctx.formParam("heladeraID");
-    System.out.println("heladeraId= "+heladeraId);
+    Integer cantidadViandas= Integer.parseInt(cantidad);
+    String heladeraIdProviene = ctx.formParam("heladeraID");
+    System.out.println("heladeraId= "+heladeraIdProviene);
+    String heladeraIdHacia = ctx.formParam("heladeraIDHacia");
+    System.out.println("heladeraIDHacia= "+heladeraIdHacia);
 
     // Convertir parámetros necesarios
-    Integer heladeraID = Integer.parseInt(heladeraId);
+    Integer heladeraID = Integer.parseInt(heladeraIdProviene);
+    Integer heladeraIDVa = Integer.parseInt(heladeraIdHacia);
 
     // Busca la heladera en la BD
     RepoHeladera hela = new RepoHeladera();
     Heladera heladera = hela.findById_Heladera(heladeraID);
     System.out.println("HelaName= "+heladera.getNombre());
+    RepoHeladera hela2 = new RepoHeladera();
+    Heladera heladeraHacia = hela2.findById_Heladera(heladeraIDVa);
+    System.out.println("Hela2Name= "+heladeraHacia.getNombre());
+
+
+    Integer i;
+    Vianda viandaHeladera;
+    List<Vianda> viandasMover= new ArrayList<>();
+    System.out.println("Cantidad de viandas de heladera2= "+ heladera.cantidadViandas());
+    RepoVianda repoVianda = new RepoVianda();
+
+
+    if(cantidadViandas<=heladera.cantidadViandas())
+    {
+        for(i=0;i<cantidadViandas;i++)
+        {
+        viandaHeladera = heladera.sacarViandaPorIndice(i);
+        viandasMover.add(viandaHeladera);
+        repoVianda.cambiarHeladera(heladeraHacia,viandaHeladera.getId());
+        }
+
+        heladeraHacia.ingresarViandas(viandasMover);
+        System.out.println("Se movieron las viandas con exito hacia la heladera" + heladeraHacia.getNombre());
+    }else  System.out.println("La heladera no puede mover mas viandas de las que tiene");
 
     // Crea Traslado
     // Funcion de traslado @Todo
