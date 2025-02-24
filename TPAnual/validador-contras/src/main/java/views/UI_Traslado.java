2ac66b9cd4b5574ea2e0fb24e9989db4df846502
Naicon67 <cerezotomas67@gmail.com>
2025-02-24 18:24:23 -0300

    Traslado

--- TPAnual/validador-contras/src/main/java/views/UI_Traslado.java
+++ TPAnual/validador-contras/src/main/java/views/UI_Traslado.java
@@ -36,99 +36,42 @@ public class UI_Traslado extends UI_Navegable implements Handler{
   }
 
   public void trasladarCantViandas(Context ctx){
-
-    System.out.println("estoy en UI_Traslado::trasladarCantViandas");
-
     // Obtener parámetros del formulario (datos enviados en la solicitud)
     String cantidad = ctx.formParam("cantidad");
-    System.out.println("cantidad= "+cantidad);
     Integer cantidadViandas= Integer.parseInt(cantidad);
     String heladeraIdProviene = ctx.formParam("heladeraID");
-    System.out.println("heladeraId= "+heladeraIdProviene);
     String heladeraIdHacia = ctx.formParam("heladeraIDHacia");
-    System.out.println("heladeraIDHacia= "+heladeraIdHacia);
     String motivoMovimiento = ctx.formParam("motivoTraslado");
-    System.out.println("el motivo de traslado es: "+ motivoMovimiento);
 
     EnumMotivosMovimientoVianda motivo=null;
-    //motivo = EnumMotivosMovimientoVianda.valueOf(motivoMovimiento)
-
-
-      if(Objects.equals(motivoMovimiento, "FALTA_DE_VIANDAS")){
-          motivo = EnumMotivosMovimientoVianda.FALTA_DE_VIANDAS;
-          System.out.println("el motivo enum es: "+ motivo);
-      } else if(Objects.equals(motivoMovimiento, "DESPERFECTO_HELADERA")){
-          motivo = EnumMotivosMovimientoVianda.DESPERFECTO_HELADERA;
-          System.out.println("el motivo enum es: "+ motivo);
-      }
-
+    if(Objects.equals(motivoMovimiento, "FALTA_DE_VIANDAS")){
+      motivo = EnumMotivosMovimientoVianda.FALTA_DE_VIANDAS;
+    } else if(Objects.equals(motivoMovimiento, "DESPERFECTO_HELADERA")){
+      motivo = EnumMotivosMovimientoVianda.DESPERFECTO_HELADERA;
+    }
 
     // Convertir parámetros necesarios
     Integer heladeraID = Integer.parseInt(heladeraIdProviene);
     Integer heladeraIDVa = Integer.parseInt(heladeraIdHacia);
-    //if(motivoMovimiento = "")
 
     // Busca la heladera en la BD
     RepoHeladera hela = RepoHeladera.getInstance();
     Heladera heladera = hela.findById_Heladera(heladeraID);
-    System.out.println("HelaName= "+heladera.getNombre());
     Heladera heladeraHacia = hela.findById_Heladera(heladeraIDVa);
-    System.out.println("Hela2Name= "+heladeraHacia.getNombre());
 
-
-    Integer i;
-    Vianda viandaHeladera;
-    List<Vianda> viandasMover= new ArrayList<>();
-    System.out.println("Cantidad de viandas de heladera origen= "+ heladera.cantidadViandas());
-    RepoVianda repoVianda = RepoVianda.getInstance();
-
-
-    if (heladera == heladeraHacia)
-    {
-      throw new IllegalArgumentException("No se puede mover a la misma heladera");
+    if (heladera == heladeraHacia) {
+        throw new IllegalArgumentException("No se puede mover a la misma heladera");
     }
 
     if(cantidadViandas<=heladera.cantidadViandas()) {
-        List<Vianda> shuffledList = new ArrayList<>(heladera.getViandasEnHeladera());
-        Collections.shuffle(shuffledList);
-        viandasMover = shuffledList.subList(0, cantidadViandas);
-        //heladera.sacarViandas(viandasMover);
-        //heladeraHacia.ingresarViandas(viandasMover);
-
-
-        System.out.println("Se movieron las viandas con exito hacia la heladera" + heladeraHacia.getNombre());
-
-        String token = ctx.cookie("Auth");
-        Claims claims = JwtUtil.getClaimsFromToken(token);
-        RepoColaborador repoColaborador = RepoColaborador.getInstance();
-        Colaborador colapinto = repoColaborador.findById_Colaborador((Integer) claims.get("roleId"));
+        Colaborador colapinto = (Colaborador) getUsuario().getRol();
 
         DistribucionVianda distribucion = new DistribucionVianda(colapinto, LocalDate.now(), heladera, heladeraHacia, cantidadViandas, motivo);
 
-
         colapinto.realizarColaboracion(distribucion);
+    } else System.out.println("La heladera no puede mover mas viandas de las que tiene");
 
-        EntityManager em = BDUtils.getEm();
-        BDUtils.comenzarTransaccion(em);
-
-        em.persist(distribucion);
-        BDUtils.commit(em);
-
-        repoVianda.cambiarHeladeraPlural(heladeraHacia,viandasMover);
-
-
-
-
-
-    }else  System.out.println("La heladera no puede mover mas viandas de las que tiene");
-
-      // Crea Traslado
-    // Funcion de traslado @Todo
-    // a la funcion de traslado se le puede pasar el usuario de la secion activa con
-    // this.getUsuario()
-
-    ctx.render("index.hbs");
+    ctx.redirect("/index");
   }
-
 
 }
